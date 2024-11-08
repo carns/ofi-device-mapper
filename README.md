a9b82ba33cf678ce4f1f7d29cd0ef95aa1566e1f
Phil Carns <carns@mcs.anl.gov>
2024-11-08 13:03:09 -0500

    initial build skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# OFI Device Mapper
+
+This repo contains experimental code for querying available CPU and NIC
+resources and generating optimal mappings.
---
+++ src/ofi-dm-query.c
@@ -0,0 +1,14 @@
+/*
+ * (C) 2024 The University of Chicago
+ *
+ * See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+int main(int argc, char **argv) {
+    return(0);
+}
