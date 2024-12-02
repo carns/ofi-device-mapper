194a9ed7313893c43f35067d500b1583fa99f556
Phil Carns <carns@mcs.anl.gov>
2024-12-02 22:20:28 +0000

    stub plumber library

---
+++ src/mochi-plumber.c
@@ -0,0 +1,12 @@
+/**
+ * @file mochi-plumber.c
+ *
+ * (C) The University of Chicago
+ *
+ * See COPYRIGHT in top-level directory.
+ */
+
+int mochi_plumber_resolve_nic(const char* in_address, const char* bucket_policy, const char* nic_policy, char** out_address) {
+
+    return(0);
+}
